e1c274d068af829fddadfa34cdddf7c699a0939d
ValimGustavo <gustavo.souza.valim@gmail.com>
2020-04-19 16:38:53 -0300

    created Patente

--- classes/entidades/Swordplay.class.ts
+++ classes/entidades/Swordplay.class.ts
@@ -1,12 +1,14 @@
 import { Data } from "../estrtutura.dados/Data";
+import { Pessoa } from './estrtutura.dados/Pessoa.class'
+import { Patente } from '../enums/Patente.enum'
 
 export class Swordplay {
   private pessoa: Pessoa;
   private data_entrada: Data;
   private data_saida: Data;
 
-  private patente;
-  private pericias;
+  private patente: Patente;
+  private pericias:[];
 
 
 }
